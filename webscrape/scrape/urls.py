1ebbac438469ff46886c3b4b98b7087e7041240b
SamuelEllenhorn <ellenhorn@chapman.edu>
2020-04-13 15:46:35 -0700

    new file

--- webscrape/scrape/urls.py
+++ webscrape/scrape/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from . import views
+rom django.conf.urls import url
+from django.contrib import admin
+from .views import signup, pagelogin, pagelogout
 
 #different views we have in this file
 urlpatterns = [
@@ -9,4 +12,7 @@ urlpatterns = [
     path("home/",views.home,name="home"),
     path("create/",views.create,name="create"),
     path("view/",views.view,name="view"),
+    path(r'^signup$', signup, name='signup'),
+    path(r'^login', pagelogin, name='login'),
+    path(r'^logout', pagelogout, name='logout'),
 ]
