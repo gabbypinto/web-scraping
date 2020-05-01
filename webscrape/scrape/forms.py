4ba20fedd01cb5abf5b0042c597cc417e0cf5905
gabbypinto <gabrielapintogp@gmail.com>
2020-05-01 08:10:22 -0700

    models update

--- webscrape/scrape/forms.py
+++ webscrape/scrape/forms.py
@@ -1,9 +1,24 @@
+from django.forms import ModelForm
 from django import forms
+from .models import Item
 
 class CreateNewList(forms.Form):
-    #there are plenty of field
     name = forms.CharField(label="Name",max_length=200)
     check = forms.BooleanField(required=False)
 
 
 #to create another form you can create another class
+class EditForm(ModelForm):
+    itemTitle = forms.CharField(label="Name",max_length=200,required = False)
+    itemPrice = forms.CharField(label="Name",max_length=200, required = False)
+    class Meta:
+        model = Item
+        fields = ["itemTitle","itemPrice"]
+        # required = False
+    # title = forms.CharField(label="Name",max_length=200,required = False)
+    # price = forms.CharField(label="Name",max_length=200, required = False)
+
+
+    #productTitle = forms.CharField(label="Name",max_length=200)
+    #price = forms.CharField(label="Name",max_length=200)
+    #checker = forms.BooleanField(required=False)
